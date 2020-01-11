d374ea754a8c54d4134909f60ab72de889397a22
Coda <coda@coda.com.ar>
2020-01-12 00:18:28 +0100

    Added soundcard argument

--- snapcastclient/run.sh
+++ snapcastclient/run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 host=$(jq -r '.host // empty' /data/options.json)
+soundcard=$(jq -r '.host // empty' /data/options.json)
 if [ -n "${host}" ];
 then
     host="-h ${host}"
 fi
-snapclient ${host} 
+if [ -n "${soundcard}" ];
+then
+    soundcard="-s ${soundcard}"
+fi
+
+snapclient ${host} ${soundcard}
